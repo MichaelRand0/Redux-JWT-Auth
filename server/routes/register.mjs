d1992ccc1d1ef6ebd798220f023817e619154676
MichaelRand0 <mihailstepanov.stepanoff2015@yandex.ru>
2023-05-28 17:00:03 +0300

    :white_check_mark: Backend: signup reworked

--- server/routes/register.mjs
+++ server/routes/register.mjs
@@ -1,8 +1,35 @@
 import { fileURLToPath } from 'node:url'
 import { join, dirname } from 'node:path'
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const file = join(__dirname, 'db.json')
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+import checkUserExists from '../helpers/checkUserExists.mjs'
+import bcrypt from 'bcryptjs'
 
-export default (req, res) => {
-  console.log('file', file)
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const file = join(__dirname, '../db.json')
+
+const adapter = new JSONFile(file)
+const defaultData = { users: [] }
+const db = new Low(adapter, defaultData)
+
+export default async (req, res) => {
+  await db.read()
+  const { login, password } = req.body
+  if (checkUserExists(login, password)) {
+    res.status(500).json({
+      message: 'User already exists',
+    })
+  } else {
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    const newUser = {
+      login,
+      password: hash,
+    }
+    db.data.users.push(newUser)
+    await db.write()
+    res.status(200).json({
+      message: 'Account successfully created!',
+    })
+  }
 }
